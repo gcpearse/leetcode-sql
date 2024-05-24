2c99dc3a946a94599119f4642df927efb504a9b9
gcpearse <glen.c.pearse@gmail.com>
2024-05-24 16:50:59 +0100

    adds query solution

---
+++ easy/0183-customers-who-never-order.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS leetcode;
+CREATE DATABASE leetcode;
+
+\c leetcode;
+
+CREATE TABLE customers (
+  id INT,
+  name VARCHAR(255)
+);
+
+CREATE TABLE orders (
+  id INT,
+  customerId INT
+);
+
+INSERT INTO customers
+  (id, name)
+VALUES
+  (1, 'Joe'),
+  (2, 'Henry'),
+  (3, 'Sam'),
+  (4, 'Max');
+
+INSERT INTO orders
+  (id, customerId)
+VALUES
+  (1, 3),
+  (2, 1);
+
+SELECT name AS customers
+FROM customers
+LEFT JOIN orders
+ON customers.id = orders.customerId
+WHERE orders.customerId IS NULL;
+
+\c postgres
+
+DROP DATABASE leetcode;
+
+\q
