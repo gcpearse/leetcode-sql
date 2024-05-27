794a1e7eb72427018d962a69e7f3b79ab5f6cce9
gcpearse <glen.c.pearse@gmail.com>
2024-05-27 23:07:22 +0100

    adds query solution

---
+++ easy/0197-rising-temperature.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS leetcode;
+CREATE DATABASE leetcode;
+
+\c leetcode;
+
+CREATE TABLE weather (
+  id INT,
+  recordDate DATE,
+  temperature INT
+);
+
+INSERT INTO weather
+  (id, recordDate, temperature)
+VALUES
+  (1, '2015-01-01', 10),
+  (2, '2015-01-02', 25),
+  (3, '2015-01-03', 20),
+  (4, '2015-01-04', 30);
+
+SELECT weather_1.id
+FROM weather AS weather_1
+INNER JOIN weather AS weather_2
+ON weather_1.recordDate = weather_2.recordDate + INTERVAL '1 day'
+WHERE weather_1.temperature > weather_2.temperature;
+
+\c postgres
+
+DROP DATABASE leetcode;
+
+\q
